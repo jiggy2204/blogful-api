3d558e47dacaa1e0998e6e81b4c1c482236f660c
jiggy2204 <jignacio2204@gmail.com>
2021-03-12 22:39:42 -0500

    blogful-api with testing

--- src/app.js
+++ src/app.js
@@ -5,12 +5,45 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const { NODE_ENV } = require("./config");
+const ArticlesService = require("./articles-service");
+const { default: knex } = require("knex");
 const app = express();
 const morganOption = NODE_ENV === "production";
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get("/articles", (req, res, next) => {
+  const knexInstance = req.app.get("db");
+
+  ArticlesService.getAllArticles(knexInstance)
+    .then((articles) => {
+      res.json(articles);
+    })
+    .catch(next);
+});
+
+app.get("/articles/:article_id", (req, res, next) => {
+  ArticlesService.getById(req.app.get("db"), req.params.article_id)
+    .then((article) => {
+      if (!article) {
+        return res.status(404).json({
+          error: { message: "Article doesn't exist" },
+        });
+      }
+    })
+    .then((articles) => {
+      res.json({
+        id: articles.id,
+        title: articles.title,
+        style: articles.style,
+        content: articles.content,
+        date_published: new Date(articles.date_published),
+      });
+    })
+    .catch(next);
+});
 
 //GET root page, send back 'Hello, world!' on web page
 app.get("/", (req, res) => {
